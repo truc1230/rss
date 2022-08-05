1842f104257cc7ec50edc72187337be0d9f5c4fc
truc1230 <supbig01@gmail.com>
2022-08-05 22:28:48 +0700

    update setup

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,5 +4,5 @@ module.exports = {
         defaultLocale: 'en',
         keySeparator: '.',
         localeDetection: false,
-    },
+    }
 };
--- src/redux/actions/nao.js
+++
@@ -1,6 +0,0 @@
-import * as types from '../actions/types';
-export const requestNao = () => async (dispatch) => {
-    dispatch({
-        type: types.NAO_STAKE_SUCCESS,
-    });
-};
